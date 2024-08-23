fcd4eb5eedc9b67ad4acbeade14b64b2e4a97611
marta16g <martugutierrez166@gmail.com>
2024-08-23 11:44:56 -0300

    3er commit

--- Program.cs
+++ Program.cs
@@ -13,4 +13,9 @@ Mostrar informe sobre la actividad de la Cadeteria.*/
 
 //CARGAR LISTADO DE CADETES POR CSV (datos precargados tipo excel separados por coma)
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Bienvenido a Cadeteria Ya!");
+Console.WriteLine("1-Alta Pedido");
+Console.WriteLine("2-Asignar Cadete");
+Console.WriteLine("3-Cambiar Estado Pedido");
+Console.WriteLine("4-Reasignar Pedido a Cadete");
+System.Console.WriteLine("5-Generar Informe Jornada.");
